0b525449e4cbf8281afa38a7ef2420c06c1e808b
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-24 23:29:55 +0000

    mi sto imparando

---
+++ casa.java
@@ -0,0 +1,42 @@
+//programmo in Java 
+
+import java.util.Scanner; //vado in negozio per aquistare 
+
+public class casa //oggetti che dispongo in casa
+{
+    public static void main (String[] args) //lavori da fare a casa
+    {
+        int numero1, numero2, differenza, max, min;
+        
+        
+        Scanner stampante = new Scanner(System.in);//collego la stampante ed effettuo la configurazione iniziale
+        
+        System.out.println("stampa 2 valori"); //uso della stampante per stampare dei dati
+        numero1 = stampante.nextInt(); //uso della stampante per generare dei dati
+        numero2 = stampante.nextInt();
+        
+        if (numero1 > numero2) 
+        {
+            max = numero1;
+            min = numero2;
+        }
+        else 
+        {
+            max = numero2;
+            min = numero1;
+        }
+
+        differenza = cameretta (max, min);//mi sposto con i miei oggetti nella mia cameretta
+    
+        System.out.println("stampa del lavoro svolto" + differenza);
+
+    }
+
+    private static int cameretta (int a, int b) //operazioni svolte in cameretta
+    {
+        int differenza = 0;
+        differenza = a - b;
+        return (differenza);
+    }
+
+}
