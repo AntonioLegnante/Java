9609c0f05115740ff6930266ddc9d9dffe7c125f
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-05 11:10:48 +0000

    progressi

--- lotta.java
+++ lotta.java
@@ -5,22 +5,48 @@ public class lotta
 {
     public static void main (String[] args)
     {
-        int colpo;
+        int energia1 = 1000, energia2 = 1000, vita1 = 800, vita2 = 800;
+        char colpo;
    
-        scontro giocatore1 = new scontro();
+        scontro giocatore1 = new scontro(energia1, vita1);
 
-        scontro giocatore2 = new scontro();
-        
+        scontro giocatore2 = new scontro(energia2, vita2);
+
+        System.out.println("pronti a combattere?");
+
         Scanner stampante = new Scanner(System.in);
-        while(true) {
+        while(giocatore1.vita > 0 && giocatore2.vita > 0) {
         
-        colpo = stampante.nextInt();
+        colpo = stampante.nextLine();
+
         switch (colpo) {
-        case 1:
-            giocatore1.colpito();
+        case 'w':
+            giocatore2.pugno();
+            giocatore1.energiapugno();
+            System.out.println(giocatore2.vita);
         break;
-        case 2:
-            giocatore2.colpito1();
+        case '8':
+            giocatore1.pugno();
+            giocatore2.energiapugno();
+            System.out.println(giocatore1.vita);
+        break;
+        case 'z':
+            giocatore1.ricarica();
+            System.out.println(giocatore1.energia);
+        break;
+        case '3':
+            giocatore2.ricarica();
+            System.out.println(giocatore2.energia);
+        break;
+        case 'd':
+            giocatore2.calcio();
+            giocatore1.energiacalcio();
+            System.out.println(giocatore2.vita);
+        break;
+        case '6':
+            giocatore1.calcio();
+            giocatore2.energiacalcio();
+            System.out.println(giocatore1.vita);
         break;
         default:
             System.out.println("hai perso");
@@ -30,23 +56,46 @@ public class lotta
         }
         }
 class scontro {
-     int energia1 = 1000;
-     int energia2 = 1000;
-     int vita1 = 800;
-     int vita2 = 800;
-    void colpito() 
+
+    public int vita, energia;
+
+    public scontro(int a, int b)
     {
-        energia1 -= 200;
-        vita2 -= 100;
-        System.out.print("stamina rimanente" + vita2);
+        vita = b;
+        energia = a;
+    }
+  
+    void pugno() 
+    {
+        vita-= 100;
+    }
+
+    void energiapugno()
+    {
+        energia -= 150;
+    }
+
+    void ricarica()
+    {
+        energia += 100;
+    }
+
+    void calcio()
+    {
+        vita -= 250;
+    }
+
+    void energiacalcio()
+    {
+        energia -= 300;
     }
     
-    void colpito1()
+    /*void colpito1()
     {
-        energia2 -= 200;
-        vita1 -= 100;
-        System.out.print("stamina rimanente" + vita1);
-    }
+        vita-= 200;
+        energia-= 100;
+
+    }*/
 }
     
             
